77f7e57504fc8cd2e3c4bb80f67a48847650150d
Squaredius <52355024+Haborym99@users.noreply.github.com>
2022-12-13 01:58:37 +0100

    Base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+function App(props) {
+  const [allProjects, setAll] = useState(props.projectsDB);
+  console.log("All project:");
+  console.log(allProjects);
+  const [focusOnProject, setFocus] = useState(allProjects[0].name);
+  console.log("Focus goes on:");
+  console.log(focusOnProject);
+  function handleFocus(choice) {
+    setFocus(choice.name);
+  }
+  const listOfProject = allProjects.map((project) => (
+    <li key={project.key} onClick={() => handleFocus(project)}>
+      {project.name}
+    </li>
+  ));
   return (
     <div className="App">
+      <div className="left-side">
+        <h2>About me:</h2>
+        <h2>Where you can find me:</h2>
+      </div>
+      <div className="center">
+        <h2>Focus on: {focusOnProject}</h2>
+      </div>
+      <div className="right-side"></div>
+      <ul role="list" className="project-array">
+        {listOfProject}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+const DATA = [
+  { id: 1, key: 1, name: "projet 1" },
+  { id: 2, key: 2, name: "projet 2" },
+  { id: 3, key: 3, name: "projet 3" },
+];
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App projectsDB={DATA} />
   </React.StrictMode>
 );
