f6f823975f110625952740d233767c0f33806b01
Squaredius <52355024+Haborym99@users.noreply.github.com>
2022-12-14 17:47:46 +0100

    Style in progress

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import Style from "./components/style";
-import "./App.css";
 import { useState } from "react";
-
 function App(props) {
-  const [theme, setTheme] = useState("BASE");
-  const [allProjects, setAll] = useState(props.projectsDB);
-  //?Mettre setTheme dans une const avec anon function
-  function handleMode(prop) {
-    if (prop !== "light") {
-      <Style mode={prop} setTheme={{ setTheme }} />;
-    } else {
-      <Style mode={prop} setTheme={{ setTheme }} />;
-    }
-    console.log(prop);
-    console.log(theme);
-  }
+  const allProjects = props.projectsDB;
+  const [mode, setMode] = useState("light");
   const [focusOnProject, setFocus] = useState(allProjects[0].name);
+  function handleMode(props) {
+    if (props === "light") {
+      setMode("light");
+    } else {
+      setMode("dark");
+    }
+  }
   function handleFocus(choice) {
     setFocus(choice.name);
   }
@@ -25,22 +20,14 @@ function App(props) {
     </li>
   ));
   return (
-    <div className="App" style={{ theme }}>
+    <div>
       <button onClick={() => handleMode("light")}>Light mode</button>
       <button onClick={() => handleMode("dark")}>Dark mode</button>
-      <div className="left-side">
-        <h2>About me:</h2>
-        <p className="about-me"></p>
-        <h2>Where you can find me:</h2>
-        <p className="contact"></p>
-      </div>
-      <div className="center">
-        <h2>Focus on: {focusOnProject}</h2>
-      </div>
-      <div className="right-side"></div>
-      <ul role="list" className="project-array">
-        {listOfProject}
-      </ul>
+      <Style
+        listOfProject={listOfProject}
+        focusOnProject={focusOnProject}
+        mode={mode}
+      />
     </div>
   );
 }
--- src/components/style.js
+++ src/components/style.js
@@ -1,9 +1,34 @@
 export default function Style(props) {
-  const lightMode = { backgroundColor: "white" };
-  const nightMode = { backgroundColor: "black" };
-  if (props.mode !== "light") {
-    return props.setTheme({ nightMode });
-  } else {
-    return props.setTheme({ lightMode });
-  }
+  const lightMode = (
+    <div className="App" style={{ backgroundColor: "white" }}>
+      <div className="left-side">
+        <h2>About me:</h2>
+        <p className="about-me"></p>
+        <h2>Where you can find me:</h2>
+        <p className="contact"></p>
+      </div>
+      <div className="center">
+        <h2>Focus on: {props.focusOnProject}</h2>
+      </div>
+      <div className="right-side"></div>
+      <ul className="project-array">{props.listOfProject}</ul>
+    </div>
+  );
+
+  const nightMode = (
+    <div className="App" style={{ backgroundColor: "black", color: "white" }}>
+      <div className="left-side">
+        <h2>About me:</h2>
+        <p className="about-me"></p>
+        <h2>Where you can find me:</h2>
+        <p className="contact"></p>
+      </div>
+      <div className="center">
+        <h2>Focus on: {props.focusOnProject}</h2>
+      </div>
+      <div className="right-side"></div>
+      <ul className="project-array">{props.listOfProject}</ul>
+    </div>
+  );
+  return props.mode === "light" ? lightMode : nightMode;
 }
